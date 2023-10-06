3e1ff7fa3a8c378e550dc4e3e19e5507022d9fb3
udinocera <udinocera@gmail.com>
2023-10-07 00:39:41 +0200

    fine 30

--- MyApp/src/Counter.jsx
+++ MyApp/src/Counter.jsx
@@ -6,6 +6,9 @@ export function Counter(){
         setCount(count + 1)
     }
 
+    //FUNZIONE Dentro setCount se dobbiamo fare + Operazioni
+    //Altrimenti se dobbiamo fare una sola operazione utilizziamo un valore immediato
+
     return(
         <div>
             <h2>{count}</h2>
