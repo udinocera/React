814be1d2586ecfeea3bdaf82ce8c33629e4a6578
udinocera <udinocera@gmail.com>
2023-10-06 23:47:08 +0200

    fine 28

--- MyApp/src/AlertClock.jsx
+++ MyApp/src/AlertClock.jsx
@@ -1,11 +1,9 @@
-export function AlertClock() {
-    function handleButtonClick(){
-        alert("Sono le: " + new Date().toLocaleTimeString())
-    }
+// eslint-disable-next-line react/prop-types
+export function AlertClock( {event}) {
     return(
         <div>
             <p>Click the button to see the current TIME</p>
-            <button onClick={handleButtonClick}>Click Me</button>
+            <button onClick={event}>Click Me</button>
         </div>
     )
 }
--- MyApp/src/App.jsx
+++ MyApp/src/App.jsx
@@ -1,10 +1,14 @@
 import { AlertClock } from "./AlertClock";
 
 function App() {
+
+  function handleButtonClick(){
+    alert("Sono le " + new Date().toLocaleTimeString())
+  }
   
   return (
     <>
-      <AlertClock />
+      <AlertClock event={handleButtonClick}/>
     </>
 
   );
