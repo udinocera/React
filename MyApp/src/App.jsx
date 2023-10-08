13278b589ebe454e02a4891eb72accdb321b63d7
udinocera <udinocera@gmail.com>
2023-10-08 23:00:06 +0200

    fine 36

--- MyApp/src/App.jsx
+++ MyApp/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
       <Clock />
 
-      <MouseClicker />
+      <MouseClicker name="one"/>
     </>
 
   )
--- MyApp/src/MouseClicker.jsx
+++ MyApp/src/MouseClicker.jsx
@@ -1,10 +1,14 @@
-export function MouseClicker() {
-    function handleButtonEvent(event){
-        console.log(event.target.name)
-    }
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+export function MouseClicker(props) {
+    const handleClick = (event) => {
+        console.log(event.target.name);
+    };
     return(
-        <div>
-            <button name="bottone"onClick={handleButtonEvent}>Clicca qui per vedere come si chiama il bottone</button>
-        </div>
+        <>
+        <button name={props.name} onClick={handleClick}>Click Me</button>
+        </>
     )
 }
