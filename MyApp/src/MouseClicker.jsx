3ff53f558b65e6bfcd3d4e3f7bb428a5b65c4a2e
udinocera <udinocera@gmail.com>
2023-10-08 23:09:47 +0200

    fine 37

--- MyApp/src/MouseClicker.jsx
+++ MyApp/src/MouseClicker.jsx
@@ -1,14 +1,17 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
+import Cuore from "/cuore.png"
 
 export function MouseClicker(props) {
     const handleClick = (event) => {
-        console.log(event.target.name);
+        console.log(event.currentTarget.name);
     };
     return(
         <>
-        <button name={props.name} onClick={handleClick}>Click Me</button>
+        <button name={props.name} onClick={handleClick}>Click Me
+        <img src={Cuore} alt="icon" />
+        </button>
         </>
     )
 }
