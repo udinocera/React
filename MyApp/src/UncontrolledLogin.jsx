9e0645e41a32a7933187fcd537097790e502ab2b
udinocera <udinocera@gmail.com>
2023-10-10 10:59:20 +0200

    fine 48

--- MyApp/src/UncontrolledLogin.jsx
+++ MyApp/src/UncontrolledLogin.jsx
@@ -5,21 +5,10 @@ export function UncontrolledLogin(){
     const handleFormSubmit = (event) =>{
         event.preventDefault();
 
-        /*Vari modi
+        /*
+        1)API standard  è FormData
 
-        1)
-        const username = event.target.elements.namedItem("username").value
-       
-        2)
-        const username = event.target.elements.username.value;
-
-        3)
-        const username= event.target.username.value;
-
-        4)
-        const data = {
-            username: formData.get("username")
-        }
+        2) Gli approcci non standard includono l'interfacciamento diretto con gli input del modulo tramite name.
 
         */
 
