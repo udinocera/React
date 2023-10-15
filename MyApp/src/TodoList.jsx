3d4afecd6298704a4480fa9f86522941653832fd
udinocera <udinocera@gmail.com>
2023-10-16 01:19:00 +0200

    fine 59

--- MyApp/src/TodoList.jsx
+++ MyApp/src/TodoList.jsx
@@ -1,45 +1,55 @@
 /* eslint-disable react/jsx-key */
 import React, { useState } from "react";
 
-const TodoList = ({items}) => {
-    const [toDoInput, setToDoInput] = useState("");
-    const [toDoList, setToDoList] = useState(items);
+const TodoList = ({ items }) => {
+  const [toDoInput, setToDoInput] = useState("");
+  const [toDoList, setToDoList] = useState(items);
 
-    const handleInputChange = (event) => {
-        setToDoInput(event.target.value);
+  const handleInputChange = (event) => {
+    setToDoInput(event.target.value);
+  };
+
+  const handleAddToDo = () => {
+    setToDoList([...toDoList, toDoInput]);
+    setToDoInput("");
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleAddToDo();
     }
+  };
 
-    const handleAddToDo = () => {
-        setToDoList([
-            ...toDoList,
-            toDoInput
-        ]);
-        setToDoInput("")
-    }
+  const handleResetItems = () => {
+    setToDoList([]);
+  };
 
-    const handleKeyPress = (event) => {
-        if(event.key === "Enter") {
-            handleAddToDo();
-        }
-    }
+  const handleRemoveSingleItems = (index) => {
+    setToDoList(toDoList.filter((element, i) => i !== index));
+  };
 
-    const handleResetItems = () => {
-        setToDoList([])
-        }
+  return (
+    <>
+      <ul>
+        {toDoList.map((item, index) => (
+          <li key={index}>
+            {item}{" "}
+            <button onClick={() => handleRemoveSingleItems(index)}>
+              Elimina
+            </button>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={toDoInput}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+      />
+      <button onClick={handleAddToDo}>Add</button>
+      <button onClick={handleResetItems}>Reset</button>
+    </>
+  );
+};
 
-    return(
-        <>
-        <ul>
-            {toDoList.map((item) => <li>{item}</li>)}
-        </ul>
-        <input type="text" value={toDoInput} onChange={handleInputChange} onKeyPress={handleKeyPress} />
-        <button onClick={handleAddToDo}>Add</button>
-        <button onClick={handleResetItems}>Reset</button>
-        </>
-    )
-
-
-}
-
-
-export default TodoList
+export default TodoList;
