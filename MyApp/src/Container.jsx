b1f84239123c6068e3e15aa09d25b706cb524057
udinocera <udinocera@gmail.com>
2023-10-22 01:16:11 +0200

    fine 69

--- MyApp/src/Container.jsx
+++ MyApp/src/Container.jsx
@@ -1,11 +1,26 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react"
 import "./Container.css"
 
 export function Container ({title, children}){
+
+    const [collapsed, setCollapsed] = useState(true);
+
+    const handleToggle = () => {
+        setCollapsed((t) => !t);
+    }
+
     return(
         <div className="container">
-        <div className="titolo">{title}</div>
-        <div className="contenuto">{children}</div>
+            <div className="container-btn">
+                <button onClick={handleToggle}>Toggle</button>
+            </div>
+            {collapsed && (
+                <>
+                 <div className="titolo">{title}</div>
+                 <div className="contenuto">{children}</div>
+                </>
+            )}
         </div>
     )
 
