270c9e3b3c92f77d4a52be19bfa33e2d23a1d811
udinocera <udinocera@gmail.com>
2023-10-23 16:10:35 +0200

    correzione 22

--- MyApp/src/Welcome.jsx
+++ MyApp/src/Welcome.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
-export function Welcome ({name, age}) {
-    return ( 
-    <div>
-    <p>Welcome, {name}!</p>
-    {age && <p>You are {age} years old</p>}
-    </div>
-    );
+export function Welcome({ name, age }) {
+    
+  return (
+    <>
+      <p>Welcome, {name}!</p>
+      {age && <p>You are {age} years old</p>}
+    </>
+  );
 }
