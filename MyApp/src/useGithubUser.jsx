97027ff753c37ce76cce8874081adbf27d8df536
udinocera <udinocera@gmail.com>
2023-11-04 00:52:51 +0100

    check 90

--- MyApp/src/useGithubUser.jsx
+++ MyApp/src/useGithubUser.jsx
@@ -1,9 +1,14 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
 import useSWR from "swr"
 
 export function useGithubUser(username) {
     const fetcher = (url) => fetch(url).then(res => res.json());
+
+    if (username) {
     const { data, error, isLoading } = useSWR(`https://api.github.com/users/${username}`, fetcher)
+    return { data, error, isLoading } 
+}
 
-    return {data, error, isLoading}
-
+return { data: null, error: null, isLoading:false}
 }
